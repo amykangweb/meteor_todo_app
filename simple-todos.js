c21b0d176392b5e1d8cbd1184b11f33872898f93
amykangweb <selene6023@gmail.com>
2015-06-20 05:41:32 -0700

    initial commit

---
+++ simple-todos.js
@@ -0,0 +1,82 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+
+  Template.body.helpers({
+
+    tasks: function() {
+      if (Session.get("hideCompleted")){
+        //If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        //Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function() {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function() {
+      return Tasks.find({checked: {$ne: true}}).count();
+    },
+
+  });
+
+  // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
+  Template.body.events({
+    "submit .new-task": function(event) {
+      // This function is called when the new task form is submitted
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  //username of logged in user
+      });
+
+      // Clear form
+      event.target.text.value = "";
+
+      // Prevent default form submit
+      return false;
+    },
+
+    "change .hide-completed input": function(event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+
+  // In the client code, below everything else
+  Template.task.events({
+    "click .toggle-checked": function() {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
+
+  Accounts.ui.config({
+      passwordSignupFields: "USERNAME_ONLY"
+  });
+
+}
+
+// At the bottom of simple-todos.js, outside of the client-only block
+Meteor.methods({
+  addTask: function(text) {
+    // Make sure the user is logged in before inserting a task
+    if(! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+  }
+});
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
